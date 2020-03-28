65e874b6633366cea53609b615dc91f14dc93fb5
Vladboulanov <vboulanov@gmail.com>
2020-03-28 18:00:01 +0300

    Vot

--- Урок 2/Урок 2/Program.cs
+++ Урок 2/Урок 2/Program.cs
@@ -14,12 +14,8 @@ namespace Урок_2
             string[] names = new string[3];
             int[] age = new int[3];
             names[0] = Console.ReadLine();
-            names[1] = Console.ReadLine();
-            names[2] = Console.ReadLine();
             Console.WriteLine("Введите возраст");
             age[0] = int.Parse(Console.ReadLine());
-            age[1] = int.Parse(Console.ReadLine());
-            age[2] = int.Parse(Console.ReadLine());
             for(int i = 0; i < names.Length; i++)
             {
                 age[i] += 4;
