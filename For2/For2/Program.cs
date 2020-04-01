0d3778c8736f9697335da7d6555438807935844e
Vladboulanov <vboulanov@gmail.com>
2020-04-01 18:12:42 +0300

    For

---
+++ For2/For2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите сумму первоначального взноса в рублях");
+                decimal sum = decimal.Parse(Console.ReadLine());
+                int cht = 0;
+                decimal procent = decimal.Parse(Console.ReadLine());
+                int desired = int.Parse(Console.ReadLine());
+                for (int i = 0; i < desired; i++)
+                {
+                    sum += procent;
+                    cht++;
+                }
+                Console.WriteLine("Необходимое количество дней  для накопления желаемой суммы:{0} ", cht);
+            }
+            catch
+            {
+                Console.WriteLine("Некорректный ввод!");
+            }
+        }
+    }
+}
