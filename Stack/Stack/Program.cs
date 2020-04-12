3635e2e3b7530da8de048e912975398441e3c3ee
Vladboulanov <vboulanov@gmail.com>
2020-04-12 17:28:37 +0300

    Stack

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            char[] s1 = s.ToCharArray(); 
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0;i<s.Length;i++)
+            {
+                    if(s1[i] == '(' ||  s1[i] == '[')
+                    {
+                        stack.Push(s1[i]);                        
+                    }
+
+                else if (s1[i] == ')' || s1[i] == ']')
+                {
+                    var Sp = stack.Pop();
+                    if (s1[i] == ')' && Sp == '(' || s1[i] == ']' && Sp == '[')
+                    {
+                        Console.WriteLine("True");
+                    }
+                    else
+                    {
+                        Console.WriteLine("False");
+                    }
+                }
+
+               
+
+
+            }
+
+           
+        }
+    }
+}
