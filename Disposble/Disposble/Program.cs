a59e2ea5c841709e7bbcd40e114f09777302a269
Vladboulanov <vboulanov@gmail.com>
2020-04-27 19:48:32 +0300

    Disposble

---
+++ Disposble/Disposble/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disposble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class ErrorList:IDisposable
+    {
+        public string Category { get; }
+        public List<string> Errors { get; set; }
+        public ErrorList(string category,List<string> errors)
+        {
+            Category = category;
+            Errors = errors;
+        }
+        public void Dispose()
+        {
+            Errors.Clear();
+
+        }
+    }
+}
