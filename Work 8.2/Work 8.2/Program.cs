3d0ca4cd82382970c0cee2053b2b05eb9e7b19fa
Vladboulanov <vboulanov@gmail.com>
2020-06-18 12:58:55 +0300

    Stroki2

---
+++ Work 8.2/Work 8.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите непустую строку: ");
+            var text = Console.ReadLine();
+            if (string.IsNullOrEmpty(text))
+            {
+                Console.WriteLine("Вы ввели пустую строку :( Попробуйте ещё раз: ");
+            }
+            else
+            {
+                char[] words = text.Reverse().ToArray();
+                string word1 = new string(words);
+                Console.WriteLine(word1.ToLower());
+            }
+            
+        }
+    }
+}
