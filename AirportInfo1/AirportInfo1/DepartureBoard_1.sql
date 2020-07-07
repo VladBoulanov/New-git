d1500c5764d4bb3ec51c4a00cea95d38c77273cf
Vladboulanov <vboulanov@gmail.com>
2020-07-07 16:50:36 +0300

    Create DepartureBoard_1.sql
    AirportInfo

---
+++ AirportInfo1/AirportInfo1/DepartureBoard_1.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[DepartureBoard]
+(
+[NumberRace] [int]  NOT NULL,
+[CityAndCounntry] [VARCHAR] (500) NOT NULL,
+[Dates] [datetime]  NOT NULL,
+[Dateflight] [date]  NOT NULL,
+[Aviacompany] [varchar] (50) NOT NULL,
+[PaneModel] [varchar] (50) NOT NULL,
+	[Id] [int] NOT NULL PRIMARY KEY
+);
+GO
+INSERT INTO [dbo].[DepartureBoard] VALUES (23, 'Moscow,Russia-Paris,France', '2019-03-20T11:25:32,2019-03-20T13:15:28', '01:49:56', 'Aeroflot', 'Boing 777', 23);
+GO
+INSERT INTO [dbo].[DepartureBoard] VALUES (23,'Tula,Russia-Berlin,Germany','2018-04-20T12:25:32,2018-04-20T15:15:28','01:49:56','Aeroflot','Boing 777');
+GO
+SELECT*FROM [dbo].[DepartureBoard];
+GO
+DROP DATABASE AirportInfo1
